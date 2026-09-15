1e33385c1a5636b68aecb3b5acca5517244b2287
btox2000-design <btox2000@gmail.com>
2026-09-15 16:05:11 -0600

    Reparacion de inicio de sesion v7

--- v2/vite.config.js
+++ v2/vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'login.html'),
+        clientes: resolve(__dirname, 'src/views/clientes.html'),
+        dinero: resolve(__dirname, 'src/views/dinero.html'),
+        loanDetails: resolve(__dirname, 'src/views/loan-details.html'),
       },
     },
   },
